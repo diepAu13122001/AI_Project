66f273978a1e35664a935e8039b0248e474654b6
diepAu13122001 <91660142+diepAu13122001@users.noreply.github.com>
2022-04-18 17:51:30 +0700

    add HTML dir

--- NMAI-20220311T165949Z-001/NMAI/src/main/java/com/example/nmai/pages/HealthCheck.java
+++
@@ -1,5 +0,0 @@
-package com.example.nmai.pages;
-
-public class HealthCheck {
-
-}
--- NMAI-20220311T165949Z-001/NMAI/src/main/webapp/createFile.js
+++
@@ -1,6 +0,0 @@
-function createFile(data) {
-    const fslibrary = require('fs');
-    fslibrary.writeFile("D:\\newfile.txt", data, (error) => {
-        if (error) throw err;
-    })
-}
